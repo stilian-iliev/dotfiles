e4ff7518fa61f6702c45e0bdd7a3d2e482d396fc
Stilian Iliev <spr1nkler@protonmail.com>
2023-03-18 23:38:04 +0200

    post install script

---
+++ post-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Installing all necessery packages
+
+sudo zypper install qutebrowser xviewer discord steam mpv htop neofetch ranger kitty wine lutris picom redshift xarchiver opi 7zip libreoffice xrandr xset xinput
+
+opi spotify-easyrpm
+
+opi spotify-adblock
